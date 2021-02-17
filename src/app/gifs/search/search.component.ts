44ee303f3c688e24443742aa08b12eaaf021ad25
Raúl Adolfo Torres Vargas <ingrauladolfo.torresvargas@gmail.com>
2021-02-17 14:55:34 -0600

    first half of gifsApp

---
+++ src/app/gifs/search/search.component.ts
@@ -0,0 +1,20 @@
+import { Component, ElementRef, ViewChild} from '@angular/core';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent {
+  @ViewChild('txtSearch') txtSearch!:ElementRef<HTMLInputElement>;
+  constructor(private gifsService: GifsService){}
+  search(){
+    // console.log(term)
+    // console.log(this.txtSearch)
+    const value = this.txtSearch.nativeElement.value;
+    this.txtSearch.nativeElement.value = '';
+   this.gifsService.searchGifs(value)
+  }
+
+}
